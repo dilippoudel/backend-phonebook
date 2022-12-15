80f6fed317efc547bc2523a168c19730caa19a0b
Dilip <dilippoudel578@gmail.com>
2022-12-15 23:09:34 +0200

    Add CRUD operation and set up environment variables

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const Contact = require('./models/person')
+
 app.use(express.static('build'))
 app.use(cors())
 app.use(bodyParser.json())
@@ -14,28 +17,7 @@ morgan.token('body', (req, res) =>
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body'),
 )
-const persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
+
 app.get('/persons', (req, res) => {
   res.send('<h1>Everything is okey</h1>')
 })
@@ -48,46 +30,50 @@ app.get('/info', (req, res) => {
 
 // get all contacts
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Contact.find({}).then((person) => {
+    res.json(person)
+  })
 })
 
 // getting single person info
-app.get('/api/persons/:id', (req, res) => {
-  let id = req.params.id
-  const findPersons = persons.find((person) => person.id === Number(id))
-  if (!findPersons) {
-    res.status(404).send()
+app.get('/api/persons/:id', async (req, res) => {
+  const contact = await Contact.findById(req.params.id)
+  if (contact) {
+    res.send(contact)
+  } else {
+    res.status(404).end()
   }
-  res.send(findPersons)
 })
 
 // deleting the entry
-app.delete('/api/persons/:id', (req, res) => {
-  let id = Number(req.params.id)
-  persons.filter((person) => person.id !== id)
+app.delete('/api/persons/:id', async (req, res) => {
+  await Contact.findByIdAndDelete(req.params.id)
   res.status(204).end()
 })
 
 // creating a contact
 app.post('/api/persons', (req, res) => {
-  let id = Math.floor(Math.random() * (10000000000 - 4))
-  let newContact = req.body
-  newContact.id = id
-  let isAlreadyExisted = persons.find((person) => person.name === req.body.name)
-  if (isAlreadyExisted) {
-    res.json({ error: 'name must be unique' }).end()
+  const body = req.body
+  if (body === undefined) {
+    return res.status(400).json({ error: 'content missing' })
   }
-  let { name } = req.body
-  let { number } = req.body
-  if (!name || !number) {
-    res.json({ error: 'name and number must be filled' }).end()
-  }
+  const person = new Contact({
+    name: body.name,
+    number: body.number,
+  })
+  person.save().then((savedPerson) => res.json(savedPerson))
+})
 
-  if (name && number && !isAlreadyExisted) {
-    persons.concat(newContact)
-  }
-
-  res.json(newContact)
+// updating the contact
+app.put('/api/persons/:id', async (req, res) => {
+  const newContact = Contact.findByIdAndUpdate(
+    req.params.id,
+    { $set: req.body },
+    (err) => {
+      if (err) return res.json({ error: 'Could not update, sth wrong' })
+      res.send('Contact updated')
+    },
+  )
 })
 
 const port = 3001

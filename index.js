7f5fb49ead31f09ff13d8b02c47f54a395ce8701
Dilip <dilippoudel578@gmail.com>
2022-12-17 15:43:57 +0200

    Merged branch exercise-3.15

--- index.js
+++ index.js
@@ -29,33 +29,39 @@ app.get('/info', (req, res) => {
 })
 
 // get all contacts
-app.get('/api/persons', async (req, res) => {
+app.get('/api/persons', async (req, res, next) => {
   try {
     const allContacts = await Contact.find({})
     return res.json(allContacts)
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' })
+    next(error)
+    // res.status(500).json({ error: 'Internal server error' })
   }
 })
 
 // getting single person info
-app.get('/api/persons/:id', async (req, res) => {
+app.get('/api/persons/:id', async (req, res, next) => {
   try {
     const person = await Contact.findById(req.params.id)
     res.send(person)
   } catch (error) {
-    res.status(404).json({ error: 'contact could not find in server' })
+    next(error)
+    // res.status(404).json({ error: 'contact could not find in server' })
   }
 })
 
 // deleting the entry
-app.delete('/api/persons/:id', async (req, res) => {
-  await Contact.findByIdAndDelete(req.params.id)
-  res.status(204).end()
+app.delete('/api/persons/:id', async (req, res, next) => {
+  try {
+    await Contact.findByIdAndRemove(req.params.id)
+    res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
 // creating a contact
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   try {
     const body = req.body
     if (body === undefined) {
@@ -67,26 +73,39 @@ app.post('/api/persons', (req, res) => {
     })
     person.save().then((savedPerson) => res.json(savedPerson))
   } catch (error) {
-    res.json({ error: 'somthing wrong' })
+    next(error)
   }
 })
 
 // updating the contact
-app.put('/api/persons/:id', async (req, res) => {
+app.put('/api/persons/:id', async (req, res, next) => {
   try {
-    const newContact = Contact.findByIdAndUpdate(
+    const body = req.body
+    const newContact = {
+      name: body.name,
+      number: body.number,
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
       req.params.id,
-      { $set: req.body },
-      (err) => {
-        if (err) return res.json({ error: 'Could not update, sth wrong' })
-        res.send('Contact updated')
-      },
+      newContact,
+      { new: true },
     )
+    res.json({ updatedContact })
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' })
+    next(error)
   }
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.name)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' }).end()
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 const port = process.env.PORT
 
 app.listen(port)
